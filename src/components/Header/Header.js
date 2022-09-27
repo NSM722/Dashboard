65146965d0f7384c376a403ac2189bc1d34bc7f0
Natalie Sakina Mbatha <nataliembatha@gmail.com>
2022-09-27 17:59:23 +0200

    Added a Header Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <header className="mb-1 mt-5">
+        <h1 className="text-dark fw-bold">Customers</h1>
+      </header>
+    </>
+  );
+}
+
+export default Header;
+
